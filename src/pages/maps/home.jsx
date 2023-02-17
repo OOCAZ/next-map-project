405662835c70835f746d50b6dc2b8b7abe3a8d17
Zac Poorman <poortexanlegos@gmail.com>
2023-02-16 19:31:35 -0700

    got icons properly, and better loading

--- src/pages/maps/home.jsx
+++ src/pages/maps/home.jsx
@@ -1,47 +1,46 @@
-import { Map, Marker } from "pigeon-maps"
+import { Map, Marker, ZoomControl } from "pigeon-maps"
 import { ImAirplane } from 'react-icons/im';
 import React from "react"
-import data from '../../exampleData/quakes.json'
+import data from '../../exampleData/quakesSmall.json'
 
 export default function Home() {
-  console.log(data)
   const features = data.features
-  console.log(features)
-  const coords = [[0,0]]
+  const coords = [[0,0,0]]
   features.forEach( feature => {
-    coords.push([feature.geometry.coordinates[0],feature.geometry.coordinates[1]])
+    coords.push([feature.geometry.coordinates[0],feature.geometry.coordinates[1],feature.id])
   })
-  console.log(coords)
   const [center, setCenter] = React.useState([39.69960614291127, -105.08409829929842])
   const [zoom, setZoom] = React.useState(3)
   return (
     <div className="App" style={{padding: 20}}>
       <Map 
-      height={300}
-      width={300}
-      center={[center[0],center[1]]}
+      height={'80vh'}
+      width={'80vw'}
+      center={[center[0],center[1], center[2]]}
       zoom={zoom} 
       onBoundsChanged={({ center, zoom }) => { 
         setCenter(center) 
         setZoom(zoom) 
       }} 
-    >{coords.map(component => (
-    <React.Fragment key={coords[0]}>
+    >{coords.map((element, index) => (
+    <div key={coords[index][2]}>
         <Marker 
-        width={50}
-        anchor={[coords[1], coords[0]]} 
+        width={10}
+        anchor={[coords[index][1], coords[index][0]]} 
       >
-          <ImAirplane />
+      
+          <ImAirplane style={{color:"black"}}/>
       </Marker>
-    </React.Fragment>
+    </div>
 ))}
 
       <Marker 
-        width={50}
+        width={10} // this adds error zooming out and does not control size
         anchor={[39.69960614291127, -105.08409829929842]} 
       >
-          <ImAirplane />
+          <ImAirplane style={{color:"black"}}/>
       </Marker>
+      <ZoomControl />
     </Map>
     </div>
   );
