2680fcb47dac85ad8a43dae12e24ce79ce5e0e36
Zac Poorman <poortexanlegos@gmail.com>
2023-03-01 18:21:44 -0700

    added api side loading

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,29 +1,23 @@
-import { Map, Marker, ZoomControl, GeoJson } from "pigeon-maps";
+import { Map, Marker, ZoomControl, GeoJsonLoader } from "pigeon-maps";
 import { Card, Typography } from "@mui/material";
 import { ImAirplane } from 'react-icons/im';
 import React from "react";
 import data from '../exampleData/quakesSmall.json';
-import data2 from '../exampleData/airspaceTest.geo.json';
 import CardContent from "@mui/material/CardContent";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { THEME } from "@/constants";
 
 export default function Home() {
-  const geoJsonLink = "https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/2_bundeslaender/4_niedrig.geo.json"
   const features = data.features
   const coords = [[0,0,0]]
   features.forEach( feature => {
     coords.push([feature.geometry.coordinates[0],feature.geometry.coordinates[1],feature.geometry.coordinates[2],feature.id])
   })
   const [center, setCenter] = React.useState([39.69960614291127, -105.08409829929842])
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
   const [zoom, setZoom] = React.useState(3)
   return (
     <div className="App" style={{padding: 20, justifyContent: 'center', alignItems: 'center', flex: 1, flexDirection: 'column'}}>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={THEME}>
       <Card sx={{width:'85vw'}}>
         <CardContent>
           <Typography>
@@ -52,8 +46,8 @@ export default function Home() {
               <ImAirplane style={{color:"black", transform: `rotate(${parseInt(element[2])}deg)`}}/>
           </Marker>
     ))}
-          <GeoJson
-            data={data2}
+          <GeoJsonLoader
+            link="/api/airspace"
             styleCallback={(feature, hover) =>
               hover
                 ? { fill: '#93c0d099', strokeWidth: '2'}
