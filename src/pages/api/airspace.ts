e54752859fbac03633772a5143b2343c786c0aa5
Zac Poorman <poortexanlegos@gmail.com>
2023-03-03 13:38:34 -0700

    added secondary URL passthrough for rendering

--- src/pages/api/airspace.ts
+++ src/pages/api/airspace.ts
@@ -1,17 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-async function dataFetcher(){
-    const response = await fetch("http://localhost:3000/api/testloader")
-    return response.json()
+async function getJSON() {
+  return fetch('http://localhost:3000/api/testloader')
+      .then((response)=>response.json())
+      .then((responseJson)=>{return responseJson});
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const response = await fetch("http://localhost:3000/api/testloader")
-    const data = response.json()
-    console.log(data)
-  res.status(200).json(data)
+  console.log()
+  async function caller() {
+    const json = await getJSON();  // command waits until completion
+    console.log(json.hello);  
+    res.status(200).json(json)          // hello is now available
+}
+  caller()
+    
 }
--- src/pages/api/testloader.ts
+++ src/pages/api/testloader.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import data2 from '../../exampleData/airspaceTest.geo.json';
+import data2 from '../../exampleData/airspaceTest.geo.json';   
 
 export default function handler(
   req: NextApiRequest,
